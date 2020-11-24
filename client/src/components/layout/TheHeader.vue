00d3d6dfb3530c21007fbd8d49b5dc70430c64a1
unknown <luka.koscak96@gmail.com>
2020-11-24 20:25:56 +0100

    Config for global scss files added. Main App.vue configured along with main content for quiz components using css grid.

---
+++ client/src/components/layout/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header-container">
+        <h1 class="header-title">QuiZZ-WiZZ</h1>
+        <ul class="header-nav-links">
+            <li class="header-nav-link"><a href="#">About this app</a></li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .header-container{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 6rem;
+        width: 100%;
+        background-color: $primary-back-color;
+        color: $primary-text-color;
+        padding: 0 1rem;
+
+        .header-nav-links{
+        list-style-type: none;
+        font-size: 1rem;
+        }
+
+        .header-nav-link{
+            display: inline-block;
+        }
+
+        a{
+            text-decoration: none;
+            color: $primary-text-color;
+        }
+    }
+</style>
---
+++ client/src/components/quiz_area/MainQuizArea.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="quiz-container">
+        <div class="quiz-question">Answer to this question?</div>
+        <div class="quiz-questions">questions</div>
+        <div class="quiz-answers">answers</div>
+        <div class="quiz-jokers">jokers</div>
+        <div class="quiz-score">score</div>
+        <div class="quiz-bid">bid</div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .quiz-container{
+        display: grid;
+        width: 100%;
+        height: 100%;
+        margin-top: 1rem;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(4, minmax(6rem, auto));
+        grid-template-areas: 
+        "question question questions"
+        "answers answers questions"
+        "jokers jokers score"
+        "... ... bid";
+        grid-gap: .5rem;
+
+        div{
+            background-color: $primary-back-color;
+            color: $primary-text-color
+        }
+
+        .quiz-question{
+            grid-area: question;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .quiz-questions{
+            grid-area: questions;
+        }
+        .quiz-answers{
+            grid-area: answers;
+        }
+        .quiz-jokers{
+            grid-area: jokers;
+        }
+        .quiz-score{
+            grid-area: score;
+        }
+        .quiz-bid{
+            grid-area: bid;
+        }
+    }
+
+</style>
